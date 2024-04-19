a9edc1d38872048654eae835f868252ab1e52207
HENRY FELIPE <henry.oliveira11@etec.sp.gov.br>
2024-04-19 20:33:14 -0300

    Adicionado arquivos antigos

--- Creative_Nexus/src/Pages/Home/Home.jsx
+++ Creative_Nexus/src/Pages/Home/Home.jsx
@@ -5,11 +5,12 @@ import styles from './styles.module.css'
 export default function Home(){
     return(
     <>
-    <section className={styles.headerdescription}>
-        <div className={styles.descriptiontitle}>
+     <section id={styles.sectionHeader}>
+      <div classNameName={styles.headerDescription}>
+        <div className={styles.descriptionTitle}>
           <h2>Creative Nexus</h2>
         </div>
-        <div className={styles.descriptiontext}>
+        <div className={styles.descriptionText}>
           <p>
             Bem-vindo à CreativeNexus, uma comunidade especializada em Minecraft
             focada na criação de builds elaboradas, desenvolvimento de sistemas
@@ -20,13 +21,58 @@ export default function Home(){
             ilimitadas de criação dentro do universo do Minecraft.
           </p>
         </div>
+      </div>
     </section>
-      
-    <section>
 
-
-    </section>
-    </>
-     
+    <main>
+      <div className={styles.cardSocial}>
+        <div className={styles.socialContainer}>
+          <div className={styles.socialHeader}>
+            <div className={styles.socialDescription}>
+              <div className={styles.socialTitle}>
+                <h2>Siga-nos</h2>
+              </div>
+              <div className={styles.socialText}>
+                <p>
+                  Fique conectado com a comunidade do Creative Nexus! Junte-se a
+                  nós nas nossas redes sociais e mergulhe em um mundo de
+                  inspiração, aprendizado e colaboração.
+                </p>
+              </div>
+              <div className={styles.socialLinks}>
+                <div className={styles.btn}>
+                  <div className={styles.btnContainer}>
+                    <a href="" className={styles.btnDiscord}>Discord</a>
+                    <div className={styles.btnIcon}>
+                      <img src="../../Assets/DiscordLogo.png" />
+                    </div>
+                  </div>
+                </div>
+                <div className={styles.btn}>
+                  <div className={styles.btnContainer}>
+                    <a className={styles.btnCoutube} href="">YouTube</a>
+                    <div className={styles.btnIcon}>
+                      <img src="../../Assets/YouTubeLogo.png" />
+                    </div>
+                  </div>
+                </div>
+                <div className={styles.btn}>
+                  <div className={styles.btnContainer}>
+                    <a href="" className={styles.btnInstagram}>Instagram</a>
+                    <div className={styles.btnIcon}>
+                      <img src="../../Assets/InstagramLogo.png" />
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className={styles.socialImg}>
+              <img src="../../Assets/CreativeNexus.png" alt="" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </main>
+    </>  
     )
 }
