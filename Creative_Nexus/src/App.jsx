a9000874b708237b0c99932a885232c08bd0845f
will9191 <willian.fernando040@gmail.com>
2024-04-21 22:47:39 -0300

    contact page

--- Creative_Nexus/src/App.jsx
+++ Creative_Nexus/src/App.jsx
@@ -1,40 +1,39 @@
 //imports
-import {BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 //pages
-import Home from './Pages/Home/Home'
-import Models from './Pages/3dModels/Models'
-import Designs from './Pages/Designs/Designs'
-import Plugins from './Pages/PagePlugins/Plugin'
-import Builds from './Pages/PageBuilds/Builds'
-import Contact from './Pages/Contact/Contact'
+import Home from "./Pages/Home/Home";
+import Models from "./Pages/3dModels/Models";
+import Designs from "./Pages/Designs/Designs";
+import Plugins from "./Pages/PagePlugins/Plugin";
+import Builds from "./Pages/PageBuilds/Builds";
+import Contact from "./Pages/Contact/Contact";
 
-//components 
-import Header from './Components/Header/Header'
-import Footer from './Components/Footer/Footer'
+//components
+import Header from "./Components/Header/Header";
+import Footer from "./Components/Footer/Footer";
 
-import './App.css'
+import "./App.css";
 
 function App() {
-
   return (
-         <div className="App">
+    <div className="App">
       <BrowserRouter>
-      <Header/>
-      <div className='container'>
-        <Routes>
-          <Route path='/' element={<Home/>}/>
-          <Route path='/3dModels' element={<Models/>}/>
-          <Route path='/Designs' element={<Designs/>}/>
-          <Route path='/Plugins' element={<Plugins/>}/>
-          <Route path='/Builds' element={<Builds/>}/>
-          <Route path='/contact' element={<Contact/>}/>
-        </Routes>
-      </div>
-      <Footer/>
+        <Header />
+        <div className="container">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/3dModels" element={<Models />} />
+            <Route path="/Designs" element={<Designs />} />
+            <Route path="/Plugins" element={<Plugins />} />
+            <Route path="/Builds" element={<Builds />} />
+            <Route path="/contact" element={<Contact />} />
+          </Routes>
+        </div>
+        <Footer />
       </BrowserRouter>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
